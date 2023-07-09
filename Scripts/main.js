a18c603ca2695d6693e66a115d5cb38f56bdcb5f
Adityasoni8898 <soniaditya541@gmail.com>
2023-07-09 07:22:40 +0530

    added pages

--- Scripts/main.js
+++ Scripts/main.js
@@ -31,3 +31,40 @@ custom_slider.addEventListener('mousemove', (e) => {
     const walk = (x - startX)*1.5;
     custom_slider.scrollLeft = scrollLeft - walk;
 });
+
+
+
+const custom_slider_j = document.querySelector('.journey_elements_container');
+let isDown_j = false;
+let startX_j;
+let scrollLeft_j;
+console.log("ad");
+
+custom_slider_j.addEventListener('mousedown', (e) => {
+    isDown_j = true
+    custom_slider_j.classList.add('active');
+    scrollLeft_j = custom_slider_j.scrollLeft;
+    startX_j = e.pageX - custom_slider_j.offsetLeft;
+});
+
+custom_slider_j.addEventListener('mouseleave', () => {
+    isDown_j = false;
+    custom_slider_j.classList.remove('active');
+});
+    
+custom_slider_j.addEventListener('mouseup', () => {
+    isDown_j = false;
+    custom_slider_j.classList.remove('active');
+});
+
+custom_slider_j.addEventListener('mousemove', (e) => {
+    if(!isDown_j){
+        return;
+    }
+
+    e.preventDefault();
+    const x_j = e.pageX - custom_slider_j.offsetLeft;
+    console.log(x_j)
+    const walk_j = (x_j - startX_j)*1.5;
+    custom_slider_j.scrollLeft = scrollLeft_j - walk_j;
+});
